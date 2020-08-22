6366745fd99fff4e6e82700f13fc38cf8ac40a20
Yotam Ronen <yotronen@gmail.com>
2020-08-22 18:49:55 +0300

    handling data ingestion methods

--- routes/word-stats.route.js
+++ routes/word-stats.route.js
@@ -1,38 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const wordStatsUtil = require('../statistics-manager/statistics-manager');
+const statsManagerUtil = require('../statistics-manager/statistics-manager');
 
 
-router.post('/', function (req, res, next) {
+router.post('/', async function (req, res) {
+    console.log('POST request received at /word-stats');
     const contentType = req.header('Content-Type');
     try {
         switch (contentType) {
             case 'text/plain':
-                //TODO
+                statsManagerUtil.getInstance().addToStatistics(req.body);
                 res.status(200).send();
                 break;
             case 'text/url':
-                //TODO
+                const response  = await fetch(req.body)
+                if (!response.ok) {
+                    throw new Error('Payload URL access failed');
+                }
+                statsManagerUtil.getInstance().addToStatistics(response.text());
                 res.status(200).send();
                 break;
+            case 'application/octet-stream':
+                const text = req.body.text();
+                statsManagerUtil.getInstance().addToStatistics(text);
+                break;
             default:
-                //TODO
+                console.log('Invalid Content-Type');
                 res.status(415).send();
                 break;
         }
     } catch (error) {
-        //TODO
+        console.error(error.message);
+        res.status(500).send();
     }
 });
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
+    console.log('GET request received at /word-stats');
     if (req.accepts('application/json')) {
         res.status(400).send();
     } else {
         try {
-            const result = wordStatsUtil.getInstance().getWordStatistics(req.query['word']);
+            const result = statsManagerUtil.getInstance().getWordStatistics(req.query['word']);
             res.status(200).send(result);
         } catch (error) {
+            console.error(error.message);
             res.status(400).send({error: error.message});
         }
     }
